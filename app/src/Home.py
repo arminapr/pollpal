195455d355955cb3e50ed08281c0f8481b0bdcca
arminapr <arminaparva@gmail.com>
2024-08-11 12:36:42 -0400

    logo and Sarah home page

--- app/src/Home.py
+++ app/src/Home.py
@@ -23,13 +23,13 @@ if st.button("Act as John, a Political Strategy Advisor",
     st.session_state['first_name'] = 'John'
     st.switch_page('pages/00_Pol_Strat_Home.py')
 
-if st.button('Act as Mohammad, an USAID worker', 
+if st.button('Act as Sarah Sullivan, a voter', 
             type = 'primary', 
             use_container_width=True):
     st.session_state['authenticated'] = True
-    st.session_state['role'] = 'usaid_worker'
-    st.session_state['first_name'] = 'Mohammad'
-    st.switch_page('pages/10_USAID_Worker_Home.py')
+    st.session_state['role'] = 'voter'
+    st.session_state['first_name'] = 'Sarah'
+    st.switch_page('pages/10_Voter.py')
 
 if st.button('Act as System Administrator', 
             type = 'primary', 
