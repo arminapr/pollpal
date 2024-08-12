2529f17f9c2e3060095ee71caa35292c3b2ec8ba
nalika_palayoor <nalika.palayoor@gmail.com>
2024-08-12 13:12:29 -0400

    ml model page (not all questions)

---
+++ api/backend/ml_models/ml_model.py
@@ -0,0 +1,7 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+import warnings
+warnings.filterwarnings("ignore")
