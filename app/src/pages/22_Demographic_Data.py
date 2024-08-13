1fea53ffdb7d29dbda66fc33be9243f62daeed53
SriVupps <sriya.vuppala12478@gmail.com>
2024-08-13 14:18:47 -0400

    delete invalid data and voter demographic pages for michelle

--- app/src/pages/22_Demographic_Data.py
+++ app/src/pages/22_Demographic_Data.py
@@ -3,26 +3,41 @@ logger = logging.getLogger(__name__)
 import streamlit as st
 from modules.nav import SideBarLinks
 import requests
+import matplotlib.pyplot as plt
 
 st.set_page_config(layout = 'wide')
 
 SideBarLinks()
 
-st.title('Voter Demographic Page')
+st.title('View Voter Demographics')
 
 st.write('\n\n')
-st.write('## Model 1 Maintenance')
 
-st.button("Train Model 01", 
-            type = 'primary', 
-            use_container_width=True)
+demographic = st.selectbox( 'Which voter demographic would you like insights into?',
+ ('gender', 'ethnicity', 'age'))
 
-st.button('Test Model 01', 
-            type = 'primary', 
-            use_container_width=True)
+# Define the API endpoint based on the selection
+if demographic == 'ethnicity':
+    endpoint = '/voter-info-ethnicity'
+elif demographic == 'age':
+    endpoint = '/voter-info-age'
+else:
+    endpoint = '/voter-info-gender'
 
-if st.button('Model 1 - get predicted value for 10, 25', 
-             type = 'primary',
-             use_container_width=True):
-  results = requests.get('http://api:4000/c/prediction/10/25').json()
-  st.dataframe(results)
+
+base_url = 'http://api:4000/d'
+results = requests.get(base_url+endpoint).json()
+data = results
+
+
+# Extract labels and values from the data
+labels = [item[demographic] for item in data]
+values = [item['userCount'] for item in data]
+
+# Plot the pie chart
+fig, ax = plt.subplots()
+ax.pie(values, labels=labels, autopct='%1.1f%%')
+ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+# Display the pie chart in Streamlit
+st.pyplot(fig)
