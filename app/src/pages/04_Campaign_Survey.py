33e2d563fbdebccf2ab32432a541505caef6167e
arminapr <arminaparva@gmail.com>
2024-08-13 15:00:53 -0400

    campaign survey feedback

--- app/src/pages/04_Campaign_Survey.py
+++ app/src/pages/04_Campaign_Survey.py
@@ -12,7 +12,17 @@ SideBarLinks()
 
 st.title('Site Feedback')
 
+response = requests.get('http://api:4000/c/campaign-id')
+
+if response.status_code == 200:
+    campaign_ids_dict = response.json() 
+    campaignIds = sorted([item['campaignId'] for item in campaign_ids_dict])
+else:
+    st.error(f"Failed to retrieve campaign IDs. Status code: {response.status_code}")
+    campaignIds = []
+
 with st.form(key='input_form'):
+  campaignId = st.selectbox("Select Campaign ID", campaignIds)
   discoveredWhere = st.text_input("Where did you discover our page?")
   addAdditionalData = st.text_area("What additional data would have been helpful for PollPal to provide?")
   isDataUseful = st.radio("Was the data provided by PollPal useful for your campaign efforts?", ('True', 'False'))
@@ -22,6 +32,7 @@ with st.form(key='input_form'):
 
 if submitted:
   data = {}
+  data['campaignId'] = campaignId
   data['discoveredWhere'] = discoveredWhere
   data['addAdditionalData'] = addAdditionalData
   data['isDataUseful'] = isDataUseful
@@ -29,5 +40,8 @@ if submitted:
   data['isUserFriendly'] = isUserFriendly
   st.write(data)
   
-  requests.post(f'http://api:4000/c/campaign-site-survey/', json=data)
-  
+  response = requests.post('http://api:4000/c/campaign-site-survey', json=data)
+  if response.status_code == 200:
+    st.success('Campaign survey response submitted!')
+  else:
+    st.error(f"Failed to submit survey. Status code: {response.status_code}")
