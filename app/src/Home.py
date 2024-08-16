0251f977794b6b5b11ab3a324291b0181232224e
SriVupps <sriya.vuppala12478@gmail.com>
2024-08-15 22:43:18 -0400

    home page comments

--- app/src/Home.py
+++ app/src/Home.py
@@ -33,6 +33,7 @@ st.title(':red[PollPal] - :blue[the Election Analysis App]')
 st.write('\n\n')
 st.write('### Welcome! As which user would you like to log in?')
 
+# creating button to mimic campaign manager log-in
 if st.button("Act as Alex Smith, a Campaign Manager",
             type = 'primary', 
             use_container_width=True):
@@ -41,6 +42,7 @@ if st.button("Act as Alex Smith, a Campaign Manager",
     st.session_state['first_name'] = 'Alex'
     st.switch_page('pages/00_Campaign_Manager.py')
 
+# creating button to mimic voter log-in
 if st.button('Act as Sarah Sullivan, a Voter', 
             type = 'primary', 
             use_container_width=True):
@@ -49,6 +51,7 @@ if st.button('Act as Sarah Sullivan, a Voter',
     st.session_state['first_name'] = 'Sarah'
     st.switch_page('pages/10_Voter.py')
 
+# creating button to mimic data-analyst log in
 if st.button('Act as Michelle Rang, a Poll-Pal Data Analyst',
             type = 'primary', 
             use_container_width=True):
